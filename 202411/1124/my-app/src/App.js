5a8c361bc1eaf9955c9d84e1d9ba63eac0534999
nandemotoken <nandemotoken@gmail.com>
2024-11-25 09:05:28 +0900

    up 1124

---
+++ 202411/1124/my-app/src/App.js
@@ -0,0 +1,18 @@
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import Main from "./Main";
+
+
+const App = () => (
+  <DynamicContextProvider
+    theme="auto"
+    settings={{
+      environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+      walletConnectors: [EthereumWalletConnectors],
+    }}
+  >
+    <Main />
+  </DynamicContextProvider>
+);
+
+export default App;
