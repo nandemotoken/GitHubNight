1a42cd15527f5f8f346c8fad4bbdc3df35564aab
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2025-01-25 10:28:18 +0900

    add gamen on at mezamashimac

--- 202501/0101/mezamashimac.sh
+++ 202501/0101/mezamashimac.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# スクリーンをオンにする
+caffeinate -u -t 1
+
 # テキストファイルからランダムに1行選択して動画URLを取得
 VIDEO_URL=$(sort -R /Users/nandemotoken/SystemDevelopment/GitHubNight/202501/0101/mezamashimac/mikuuta0108.txt | head -n 1)
 echo "$VIDEO_URL"
